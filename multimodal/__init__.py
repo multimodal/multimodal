d665ce6db9beb696b4ff762a7a7016e23228589c
Corentin Dancette <corentin.dancette@gmail.com>
2020-11-23 18:06:18 +0100

    Fix import order for default data dir

--- multimodal/__init__.py
+++ multimodal/__init__.py
@@ -1,11 +1,12 @@
-import multimodal.datasets
-import multimodal.text
-import os
 from appdirs import user_data_dir
-import multimodal.utils as utils
+import os
 
-if os.environ["MULTIMODAL_DATA_DIR"]:
+if os.environ.get("MULTIMODAL_DATA_DIR", None):
     DEFAULT_DATA_DIR = os.environ["MULTIMODAL_DATA_DIR"]
-
 else:
     DEFAULT_DATA_DIR = user_data_dir("multimodal")
+
+
+import multimodal.datasets
+import multimodal.text
+import multimodal.utils as utils
