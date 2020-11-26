e3b364801dc3525e98e7b06850d8c35f765ef688
Corentin Dancette <corentin.dancette@gmail.com>
2020-11-26 18:38:16 +0100

    Simplify tests for VQA

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -1,33 +1,18 @@
 from multimodal.datasets import VQA, VQA2, VQACP, VQACP2
 import tempfile
 
-def test_full_download():
-    with tempfile.TemporaryDirectory() as d:
-        dataset = VQA(dir_data=d, split="val")
-
 def test_datasets():
     with tempfile.TemporaryDirectory() as d:
-        for dataset in [VQA, VQA2]:
-            train = dataset(dir_data=d, split="train")
-            print(train[0])
-            val = dataset(dir_data=d, split="val")
-            print(val[0])
-            test = dataset(dir_data=d, split="test")
-            print(test[0])
+        train = VQA(dir_data=d, split="train")
+        print(train[0])
+        val = VQA(dir_data=d, split="val")
+        print(val[0])
+        test = VQA(dir_data=d, split="test")
+        print(test[0])
 
-def test_datasets_cp():
-    with tempfile.TemporaryDirectory() as d:
-        for dataset in [VQACP, VQACP2]:
-            train = dataset(dir_data=d, split="train")
-            print(train[0])
-            test = dataset(dir_data=d, split="test")
-            print(test[0])
-
-def test_vqa_eval():
-    with tempfile.TemporaryDirectory() as d:
-        vqa_val = VQA(dir_data=d, split="val")
-        print(vqa_val[0])
+        # test VQA eval
+        print(val[0])
         predicted_answers = [
-            {"question_id": qid, "answer": "yes"} for qid in vqa_val.qid_to_annot
+            {"question_id": qid, "answer": "yes"} for qid in val.qid_to_annot
         ]
-        print(vqa_val.evaluate(predicted_answers))
+        print(val.evaluate(predicted_answers))
